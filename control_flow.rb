845d747d8af2d188bb152a224c9d6ec324c868a5
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-18 10:47:51 -0500

    commit tree 689e91c25921d394da228ff40189fe3232dd7e8b

--- control_flow.rb
+++ control_flow.rb
@@ -9,16 +9,46 @@ def admin_login(username, password)
 end
 
 def hows_the_weather(temperature)
-  case temperature
-  when <40
+  if temperature < 40
     "It's brisk out there!"
+  elsif (temperature > 40 && temperature < 65) 
+    "It's a little chilly out there!"
+  elsif temperature > 85 
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end 
 end
 
-# def fizzbuzz(num)
-#   # your code here
-# end
+def fizzbuzz(num)
+  num_by_3 = num % 3 
+  num_by_5 = num % 5 
+  if (num_by_3 == 0 && num_by_5 == 0)
+    "FizzBuzz"
+  elsif num_by_3 == 0 
+    "Fizz"
+  elsif num_by_5 == 0
+    "Buzz"
+  else 
+    num
+  end
+end
 
-# def calculator(operation, num1, num2)
-#   # your code here
-# end
+def calculator(operation, num1, num2)
+  # binding.pry
+  if operation == "+"
+    num1 + num2
+  elsif operation =="-"
+    num1 - num2
+  elsif operation == "*"
+    num1*num2
+  elsif operation == "/"
+    num1/num2
+  elsif (operation != "+" || operation != "-" || operation != "*" || operation != "/")
+    puts "Invalid operation!" 
+    nil
+  end
+end
+
+puts calculator('tits', 2, 3)
 
